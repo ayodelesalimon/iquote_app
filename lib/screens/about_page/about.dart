ed7fc1a62964c0a7609813802d14a9827047ee84
ayodelesalimon <ayodele951@gmail.com>
2021-01-20 16:36:19 +0200

    new

--- lib/screens/about_page/about.dart
+++ lib/screens/about_page/about.dart
@@ -1,1 +1,73 @@
+import 'package:flut/widgets/flexible_appbar.dart';
 import 'package:flutter/material.dart';
+
+class AboutPage extends StatefulWidget {
+  @override
+  _AboutPageState createState() => _AboutPageState();
+}
+
+class _AboutPageState extends State<AboutPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          overflow: Overflow.visible,
+          children: [
+            Container(
+              height: 200,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage("assets/bg.png"),
+                ),
+              ),
+            ),
+            Positioned(
+              left: 30,
+              top: 170,
+              child: Container(
+                width: 350,
+                height: 100,
+                decoration: BoxDecoration(
+                  color: Colors.amber,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+            ),
+            Positioned(
+              left: 30,
+              top: 420,
+              child: Align(
+                alignment: Alignment.center,
+                child: Container(
+                  width: 350,
+                  height: 100,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              left: 30,
+              top: 295,
+              child: Align(
+                alignment: Alignment.center,
+                child: Container(
+                  width: 350,
+                  height: 100,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/morepage.dart
+++ lib/screens/morepage.dart
@@ -1,3 +1,4 @@
+import 'package:flut/screens/about_page/about.dart';
 import 'package:flutter/material.dart';
 
 class MorePage extends StatefulWidget {
@@ -10,7 +11,7 @@ class _MorePageState extends State<MorePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text("More Page"),
+        child: AboutPage(),
       ),
     );
   }
